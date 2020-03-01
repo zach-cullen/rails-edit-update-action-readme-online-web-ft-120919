20268a1600f434c4c6b59a4f6ed4623e4356ef2d
Zach Cullen <zacullen@gmail.com>
2020-03-01 10:40:48 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create(title: "This Title", description: "This can be described.")
